ef927c26d3f301b76431f77af9fb7da16b602e31
jeevzzz <101239474+jeevzzz@users.noreply.github.com>
2022-03-22 11:46:07 +0530

    Add files via upload

---
+++ Sourcecode/FlinkJobs/testflink/src/main/java/com/example/demo/FlinkKafkaDemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlinkKafkaDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FlinkKafkaDemoApplication.class, args);
+	}
+
+}
