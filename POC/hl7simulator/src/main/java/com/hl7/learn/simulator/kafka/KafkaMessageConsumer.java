6d82f96c6c683514d78b13409509b3468a175377
jeevzzz <101239474+jeevzzz@users.noreply.github.com>
2022-03-30 19:03:05 +0530

    Delete POC/hl7simulator directory

--- POC/hl7simulator/src/main/java/com/hl7/learn/simulator/kafka/KafkaMessageConsumer.java
+++
@@ -1,9 +0,0 @@
-package com.hl7.learn.simulator.kafka;
-
-/**
- * @author 1013744
- *
- */
-public class KafkaMessageConsumer {
-
-}
--- POC/hl7simulator/src/main/java/com/hl7/learn/simulator/type/WaveformParameter.java
+++
@@ -1,9 +0,0 @@
-package com.hl7.learn.simulator.type;
-
-/**
- * @author 1013744
- *
- */
-public enum WaveformParameter {
-	MDC_ECG_ELEC_POTL_I
-}
